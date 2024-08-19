63fd71a0726b3e881461ef06255495c0a2a61d5e
Mustafa Muhaimin <mumufahad@gmail.com>
2024-08-20 01:15:03 +0600

    Latest chat message sent first

--- src/main/java/com/amplifiers/pathfinder/entity/ChatMessage/ChatMessageRepository.java
+++ src/main/java/com/amplifiers/pathfinder/entity/ChatMessage/ChatMessageRepository.java
@@ -6,10 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Integer> {
-    Page<ChatMessage> findAllByChatIdOrderByTimeStampAsc(Pageable pageable, String chatId);
+    Page<ChatMessage> findAllByChatIdOrderByTimeStampDesc(Pageable pageable, String chatId);
 
     @Query("SELECT COUNT(cm) > 0 FROM ChatMessage cm WHERE cm.receiverId = :receiverId AND cm.read = false")
     boolean userHasUnreadMessages(@Param("receiverId") Integer receiverId);
