3b9a3cedea92d98233b3e7ae4108b8cf1af40232
Mustafa Muhaimin <mumufahad@gmail.com>
2024-08-29 17:58:17 +0600

    gig faq limit lessen

--- src/main/java/com/amplifiers/pathfinder/entity/gig/GigService.java
+++ src/main/java/com/amplifiers/pathfinder/entity/gig/GigService.java
@@ -1,7 +1,7 @@
 package com.amplifiers.pathfinder.entity.gig;
 
-import com.amplifiers.pathfinder.entity.gig.GigRepository.Specs;
 import com.amplifiers.pathfinder.entity.enrollment.EnrollmentRepository;
+import com.amplifiers.pathfinder.entity.gig.GigRepository.Specs;
 import com.amplifiers.pathfinder.entity.image.Image;
 import com.amplifiers.pathfinder.entity.image.ImageService;
 import com.amplifiers.pathfinder.entity.review.ReviewRepository;
@@ -19,6 +19,9 @@ import com.amplifiers.pathfinder.utility.Category;
 import com.amplifiers.pathfinder.utility.UserUtility;
 import com.amplifiers.pathfinder.utility.Variables.PaginationSettings;
 import com.recombee.api_client.bindings.RecommendationResponse;
+import java.time.OffsetDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,12 +30,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.time.OffsetDateTime;
-import java.util.*;
-import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class GigService {
+
     private final UserUtility userUtility;
 
     private final GigRepository repository;
@@ -51,10 +52,9 @@ public class GigService {
             throw new ValidationException("At least three tags required.");
         }
 
-        if (request.getFaqs().size() < 3) {
-            throw new ValidationException("At least three faqs required.");
+        if (request.getFaqs().size() < 1) {
+            throw new ValidationException("At least one faqs required.");
         }
-
     }
 
     public Gig createGig(GigCreateRequest request) {
@@ -62,26 +62,23 @@ public class GigService {
 
         gigCreateRequestValidation(request);
 
-        request.getTags().forEach(
-                name -> tagService.findByName(name).orElseGet(() -> tagService.createTag(new TagCreateRequest(name))));
+        request.getTags().forEach(name -> tagService.findByName(name).orElseGet(() -> tagService.createTag(new TagCreateRequest(name))));
 
-        Set<Tag> tags = request.getTags().stream()
-                .map(name -> tagService.findByName(name).get())
-                .collect(Collectors.toSet());
+        Set<Tag> tags = request.getTags().stream().map(name -> tagService.findByName(name).get()).collect(Collectors.toSet());
 
         var gig = Gig.builder()
-                .title(request.getTitle())
-                .description(request.getDescription())
-                .price(request.getPrice())
-                .offerText(request.getOfferText())
-                .category(request.getCategory())
-                .rating(0.0f)
-                .accepted(false)
-                .seller(user)
-                .tags(tags)
-                .faqs(request.getFaqs())
-                .createdAt(OffsetDateTime.now())
-                .build();
+            .title(request.getTitle())
+            .description(request.getDescription())
+            .price(request.getPrice())
+            .offerText(request.getOfferText())
+            .category(request.getCategory())
+            .rating(0.0f)
+            .accepted(false)
+            .seller(user)
+            .tags(tags)
+            .faqs(request.getFaqs())
+            .createdAt(OffsetDateTime.now())
+            .build();
 
         Gig savedGig = repository.save(gig);
         recommendationService.sendGigValues(savedGig);
@@ -99,13 +96,16 @@ public class GigService {
     }
 
     public GigPageDTO findById(Integer id) {
-        Gig gig = repository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Gig not found"));
+        Gig gig = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Gig not found"));
 
         Video video = gig.getGigVideo();
 
         if (video != null) {
-            if (video.getPresignedUrl() == null || video.getPresignedUrlExpire() == null || OffsetDateTime.now().isAfter(video.getPresignedUrlExpire())) {
+            if (
+                video.getPresignedUrl() == null ||
+                video.getPresignedUrlExpire() == null ||
+                OffsetDateTime.now().isAfter(video.getPresignedUrlExpire())
+            ) {
                 videoService.createVideoPresignedUrl(video);
             }
         }
@@ -114,13 +114,16 @@ public class GigService {
     }
 
     public GigPageDTO privateFindById(Integer id) {
-        Gig gig = repository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Gig not found"));
+        Gig gig = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Gig not found"));
 
         Video video = gig.getGigVideo();
 
         if (video != null) {
-            if (video.getPresignedUrl() == null || video.getPresignedUrlExpire() == null || OffsetDateTime.now().isAfter(video.getPresignedUrlExpire())) {
+            if (
+                video.getPresignedUrl() == null ||
+                video.getPresignedUrlExpire() == null ||
+                OffsetDateTime.now().isAfter(video.getPresignedUrlExpire())
+            ) {
                 videoService.createVideoPresignedUrl(video);
             }
         }
@@ -135,8 +138,7 @@ public class GigService {
     }
 
     public String deleteGig(Integer gigId) {
-        Gig gig = repository.findById(gigId)
-                .orElseThrow(() -> new ResourceNotFoundException("Gig not found"));
+        Gig gig = repository.findById(gigId).orElseThrow(() -> new ResourceNotFoundException("Gig not found"));
 
         User user = userUtility.getCurrentUser();
 
@@ -155,8 +157,7 @@ public class GigService {
     }
 
     public void editGig(GigCreateRequest request, Integer gigId) {
-        Gig gig = repository.findById(gigId)
-                .orElseThrow(() -> new ResourceNotFoundException("Gig not found"));
+        Gig gig = repository.findById(gigId).orElseThrow(() -> new ResourceNotFoundException("Gig not found"));
 
         if (!isGigOfUser(gig)) {
             throw new ValidationException("User not owner of this gig");
@@ -164,12 +165,9 @@ public class GigService {
 
         gigCreateRequestValidation(request);
 
-        request.getTags().forEach(
-                name -> tagService.findByName(name).orElseGet(() -> tagService.createTag(new TagCreateRequest(name))));
+        request.getTags().forEach(name -> tagService.findByName(name).orElseGet(() -> tagService.createTag(new TagCreateRequest(name))));
 
-        Set<Tag> tags = request.getTags().stream()
-                .map(name -> tagService.findByName(name).get())
-                .collect(Collectors.toSet());
+        Set<Tag> tags = request.getTags().stream().map(name -> tagService.findByName(name).get()).collect(Collectors.toSet());
 
         gig.setTitle(request.getTitle());
         gig.setDescription(request.getDescription());
@@ -183,8 +181,7 @@ public class GigService {
     }
 
     public Image setCoverImage(Integer gigId, MultipartFile image) throws Exception {
-        Gig gig = repository.findById(gigId)
-                .orElseThrow(() -> new ResourceNotFoundException("Gig id " + gigId + " does not exist."));
+        Gig gig = repository.findById(gigId).orElseThrow(() -> new ResourceNotFoundException("Gig id " + gigId + " does not exist."));
 
         if (!isGigOfUser(gig)) {
             throw new ValidationException("User not owner of this gig");
@@ -205,8 +202,7 @@ public class GigService {
     }
 
     public Video setGigVideo(Integer gigId, MultipartFile video) throws Exception {
-        Gig gig = repository.findById(gigId)
-                .orElseThrow(() -> new ResourceNotFoundException("Gig id " + gigId + " does not exist."));
+        Gig gig = repository.findById(gigId).orElseThrow(() -> new ResourceNotFoundException("Gig id " + gigId + " does not exist."));
 
         if (!isGigOfUser(gig)) {
             throw new ValidationException("User not owner of this gig");
@@ -231,77 +227,70 @@ public class GigService {
     }
 
     public GigPageDTO createGigPageDTO(Gig gig) {
-
         List<Float> ratingList = reviewRepository.findAllByGigId(gig.getId()).stream().map(r -> Float.valueOf(r.getRating())).toList();
 
-        float rating = ratingList.size() > 0 ? (ratingList
-                .stream()
-                .reduce((float) 0, Float::sum)) / ratingList.size() : 0;
-
+        float rating = ratingList.size() > 0 ? (ratingList.stream().reduce((float) 0, Float::sum)) / ratingList.size() : 0;
 
         return GigPageDTO.builder()
-                .id(gig.getId())
-                .title(gig.getTitle())
-                .category(gig.getCategory())
-                .description(gig.getDescription())
-                .offerText(gig.getOfferText())
-                .price(gig.getPrice())
-                .gigCoverImage(gig.getGigCoverImage() != null ? gig.getGigCoverImage().getFilename() : null)
-                .gigVideo(gig.getGigVideo() != null ? gig.getGigVideo().getPresignedUrl() : null)
-                .tags(gig.getTags().stream().map(Tag::getName).toList())
-                .faqs(gig.getFaqs())
-                .rating(rating)
-                .totalReviews(ratingList.size())
-                .totalCompleted(enrollmentRepository.countByGigAndCompletedAtNotNull(gig))
-                .totalOrders(enrollmentRepository.countByGig(gig))
-                .accepted(gig.isAccepted())
-                .seller(UserShortDTO.builder()
-                        .id(gig.getSeller().getId())
-                        .firstName(gig.getSeller().getFirstName())
-                        .lastName(gig.getSeller().getLastName())
-                        .build())
-                .createdAt(gig.getCreatedAt())
-                .build();
+            .id(gig.getId())
+            .title(gig.getTitle())
+            .category(gig.getCategory())
+            .description(gig.getDescription())
+            .offerText(gig.getOfferText())
+            .price(gig.getPrice())
+            .gigCoverImage(gig.getGigCoverImage() != null ? gig.getGigCoverImage().getFilename() : null)
+            .gigVideo(gig.getGigVideo() != null ? gig.getGigVideo().getPresignedUrl() : null)
+            .tags(gig.getTags().stream().map(Tag::getName).toList())
+            .faqs(gig.getFaqs())
+            .rating(rating)
+            .totalReviews(ratingList.size())
+            .totalCompleted(enrollmentRepository.countByGigAndCompletedAtNotNull(gig))
+            .totalOrders(enrollmentRepository.countByGig(gig))
+            .accepted(gig.isAccepted())
+            .seller(
+                UserShortDTO.builder()
+                    .id(gig.getSeller().getId())
+                    .firstName(gig.getSeller().getFirstName())
+                    .lastName(gig.getSeller().getLastName())
+                    .build()
+            )
+            .createdAt(gig.getCreatedAt())
+            .build();
     }
 
     public static GigShortDTO createGigShortDTO(Gig gig) {
         return GigShortDTO.builder()
-                .id(gig.getId())
-                .title(gig.getTitle())
-                .coverImage(gig.getGigCoverImage() != null ? gig.getGigCoverImage().getFilename() : null)
-                .build();
+            .id(gig.getId())
+            .title(gig.getTitle())
+            .coverImage(gig.getGigCoverImage() != null ? gig.getGigCoverImage().getFilename() : null)
+            .build();
     }
 
     public GigCardDTO createGigCardDTO(Gig gig, boolean includeUser) {
         List<Float> ratingList = reviewRepository.findAllByGigId(gig.getId()).stream().map(r -> Float.valueOf(r.getRating())).toList();
 
-        float rating = ratingList.size() > 0 ? (ratingList
-                .stream()
-                .reduce((float) 0, Float::sum)) / ratingList.size() : 0;
+        float rating = ratingList.size() > 0 ? (ratingList.stream().reduce((float) 0, Float::sum)) / ratingList.size() : 0;
 
         int ratedByCount = ratingList.size();
 
-
         var gigCardDTOBuilder = GigCardDTO.builder()
-                .id(gig.getId())
-                .title(gig.getTitle())
-                .price(gig.getPrice())
-                .rating(rating)
-                .ratedByCount(ratedByCount)
-                .coverImage(gig.getGigCoverImage() != null ? gig.getGigCoverImage().getFilename() : null)
-                .tags(gig.getTags().stream().map(Tag::getName).collect(Collectors.toSet()));
-
+            .id(gig.getId())
+            .title(gig.getTitle())
+            .price(gig.getPrice())
+            .rating(rating)
+            .ratedByCount(ratedByCount)
+            .coverImage(gig.getGigCoverImage() != null ? gig.getGigCoverImage().getFilename() : null)
+            .tags(gig.getTags().stream().map(Tag::getName).collect(Collectors.toSet()));
 
         if (includeUser) {
             UserShortDTO userShortDTO = UserShortDTO.builder()
-                    .id(gig.getSeller().getId())
-                    .firstName(gig.getSeller().getFirstName())
-                    .lastName(gig.getSeller().getLastName())
-                    .build();
+                .id(gig.getSeller().getId())
+                .firstName(gig.getSeller().getFirstName())
+                .lastName(gig.getSeller().getLastName())
+                .build();
 
             gigCardDTOBuilder = gigCardDTOBuilder.user(userShortDTO);
         }
-
 
         return gigCardDTOBuilder.build();
     }
@@ -321,7 +310,11 @@ public class GigService {
 
     public Object getRecommendationsForUser(String scenario) {
         User user = userUtility.getCurrentUser();
-        RecommendationResponse recommended = recommendationService.getRecommendationsForUser(user.getId(), PaginationSettings.NUM_RECOMMENDED_GIGS, scenario);
+        RecommendationResponse recommended = recommendationService.getRecommendationsForUser(
+            user.getId(),
+            PaginationSettings.NUM_RECOMMENDED_GIGS,
+            scenario
+        );
 
         return getReturnDataForRecommendation(recommended);
     }
@@ -329,20 +322,32 @@ public class GigService {
     public Object getRecommendationsForAnonymousUsers(String scenario) {
         // for globally popular gigs.
         // -1 means no user. handled in recommendation service.
-        RecommendationResponse recommended = recommendationService.getRecommendationsForUser(-1, PaginationSettings.NUM_RECOMMENDED_GIGS, scenario);
+        RecommendationResponse recommended = recommendationService.getRecommendationsForUser(
+            -1,
+            PaginationSettings.NUM_RECOMMENDED_GIGS,
+            scenario
+        );
 
         return getReturnDataForRecommendation(recommended);
     }
 
     public Object getRecommendationsForItem(Integer gigId, String scenario) {
         User user = userUtility.getCurrentUser();
-        RecommendationResponse recommended = recommendationService.getRecommendationsForItem(gigId, user.getId(), PaginationSettings.NUM_RECOMMENDED_GIGS, scenario);
+        RecommendationResponse recommended = recommendationService.getRecommendationsForItem(
+            gigId,
+            user.getId(),
+            PaginationSettings.NUM_RECOMMENDED_GIGS,
+            scenario
+        );
 
         return getReturnDataForRecommendation(recommended);
     }
 
     public Object getNextRecommendationsForUser(String recommId) {
-        RecommendationResponse recommended = recommendationService.getNextRecommendationsForUser(recommId, PaginationSettings.NUM_RECOMMENDED_GIGS);
+        RecommendationResponse recommended = recommendationService.getNextRecommendationsForUser(
+            recommId,
+            PaginationSettings.NUM_RECOMMENDED_GIGS
+        );
         return getReturnDataForRecommendation(recommended);
     }
 
@@ -356,9 +361,7 @@ public class GigService {
         }
 
         // Fetch gigs and create DTOs
-        List<GigCardDTO> recommendedGigs = repository.findAllById(List.of(recommsInt)).stream()
-                .map(this::createGigCardDTO)
-                .toList();
+        List<GigCardDTO> recommendedGigs = repository.findAllById(List.of(recommsInt)).stream().map(this::createGigCardDTO).toList();
 
         // Using a Map to hold return data
         Map<String, Object> returnData = new HashMap<>();
@@ -367,7 +370,6 @@ public class GigService {
 
         return returnData;
     }
-
 
     // INFO: HOW SEARCH WORKS.
     // when a user starts typing the query, we recommend them the tags and categories with the 'query' prefix.
@@ -382,28 +384,27 @@ public class GigService {
     // TODO: implement a scoring algorithm.
     // TODO: update all gig fetch functions to incorporate scoring.
 
-//    public List<Gig> getSimilarGigs() {
-//        // TODO
-//    }
+    //    public List<Gig> getSimilarGigs() {
+    //        // TODO
+    //    }
 
-//    public List getCategoryAndTagsForKeyword() {
-//        // TODO: return whether it's a category or tag. so we can call the corresponding function.
-//        // ie. findByCategory() or findByQuery().
-//    }
+    //    public List getCategoryAndTagsForKeyword() {
+    //        // TODO: return whether it's a category or tag. so we can call the corresponding function.
+    //        // ie. findByCategory() or findByQuery().
+    //    }
 
     public Page<Gig> findByCategory(Pageable pageable, String category) {
         return repository.findByCategory(pageable, category);
     }
 
     public Page<GigCardDTO> findByQuery(
-            Pageable pageable,
-            String query,
-            Float ratingAbove,
-            Float budget,
-            Category category,
-            List<String> tagStrings
-            ) {
-
+        Pageable pageable,
+        String query,
+        Float ratingAbove,
+        Float budget,
+        Category category,
+        List<String> tagStrings
+    ) {
         Specification<Gig> specification = Specs.isLike(query).and(Specs.isRatingAbove(ratingAbove));
 
         if (budget != null) {
@@ -415,10 +416,10 @@ public class GigService {
         }
 
         if (tagStrings != null && !tagStrings.isEmpty()) {
-            List<Tag> tags = tagStrings.stream()
-                    .map(t->tagService.findByName(t)
-                            .orElseThrow(()-> new ResourceNotFoundException("Tag doesn't exist")))
-                    .toList();
+            List<Tag> tags = tagStrings
+                .stream()
+                .map(t -> tagService.findByName(t).orElseThrow(() -> new ResourceNotFoundException("Tag doesn't exist")))
+                .toList();
 
             specification = specification.and(Specs.hasTags(tags));
         }
@@ -427,18 +428,16 @@ public class GigService {
 
         return gigs.map(g -> createGigCardDTO(g, true));
     }
+    //    public List<Gig> findByTag(String tag) {
+    //        // TODO
+    //    }
 
-
-//    public List<Gig> findByTag(String tag) {
-//        // TODO
-//    }
-
-//    public List<Gig> getFeaturedGigs() {
-//        // TODO
-//    }
-//
-//    // For explore new talents feature.
-//    public List<Gig> getNewGigs() {
-//        // TODO
-//    }
+    //    public List<Gig> getFeaturedGigs() {
+    //        // TODO
+    //    }
+    //
+    //    // For explore new talents feature.
+    //    public List<Gig> getNewGigs() {
+    //        // TODO
+    //    }
 }
