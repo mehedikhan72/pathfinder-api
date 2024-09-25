aa770001f24624f54164607cecf4cf1c0c14ab39
Mustafa Muhaimin <mumufahad@gmail.com>
2024-09-25 14:13:34 +0600

    private gig bug

--- src/main/java/com/amplifiers/pathfinder/entity/gig/GigService.java
+++ src/main/java/com/amplifiers/pathfinder/entity/gig/GigService.java
@@ -129,7 +129,7 @@ public class GigService {
         Gig gig = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Gig not found"));
 
         User currentUser = userUtility.getCurrentUser();
-        if (!Objects.equals(currentUser.getId(), gig.getSeller().getId())) {
+        if (!Objects.equals(currentUser.getId(), gig.getSeller().getId()) && (!gig.isAccepted() || gig.isPaused())) {
             throw new ResourceNotFoundException("Gig Not Found");
         }
 
